95e7bc3a4ee35e980eddd61c0340b0c24fbce489
Samuel Nichols <Nichols_Samuel@outlook.com>
2022-01-30 23:12:18 -0600

    Asking user for account number

--- CSF1Homework/Program.cs
+++ CSF1Homework/Program.cs
@@ -10,6 +10,37 @@ namespace CSF1Homework
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+
+            // Ask the user to enter an account number for their account
+            //Continue to ask the user for their account number until they get it right (the correct account number will be hard coded in your code. See the login section for an example.
+
+
+             Console.WriteLine("Please enter your account number: ");
+             string userName = Console.ReadLine();
+             int accountNumber = Convert.ToInt32(userName);
+
+             bool isUser = false;
+
+             if (accountNumber == 123456789)
+             {
+                 isUser = true;
+             }
+
+             if (isUser)
+             {
+                 Console.WriteLine("Access Granted! ");
+             }
+             else
+             {
+                 Console.WriteLine("Access Denied, Please enter a Valid account. ");
+             }
+
+
+
+            //Create Loop
+
+        }//End Main
+
+    }//End Class
+
+}//End Namespace
